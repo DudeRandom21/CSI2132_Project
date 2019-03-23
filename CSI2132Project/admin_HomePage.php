159910f791b72e576df0f5734fae824b451c7041
Nicolas Pare <npare056@uottawa.ca>
2019-03-23 17:22:01 -0400

    New admin home page.

---
+++ CSI2132Project/admin_HomePage.php
@@ -0,0 +1,17 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<?php include('common_head.php'); ?>
+</head>
+
+<body>
+    <div>
+    </div>
+
+<?php include("header.php") ?>    
+
+</body>
+
+</html>
