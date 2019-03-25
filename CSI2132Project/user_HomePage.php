e2507e57c9094890173d22114ff414d217208bd3
Jonathan <DudeRandom21@gmail.com>
2019-03-24 22:01:50 -0400

    started building complex SQL query for room search

--- CSI2132Project/user_HomePage.php
+++ CSI2132Project/user_HomePage.php
@@ -3,7 +3,35 @@
 <html lang="en">
 
 <head>
-<?php include("common_head.php"); ?>
+<?php include("common_head.php");
+    include("scrollable_table.php"); 
+
+    $db_connection = pg_connect("host=localhost dbname=csi2132_project user=web password=webapp");
+    if (!empty($_GET)) {
+
+        $tableQuery = "SELECT * FROM hotel_chain JOIN hotel ON hotel_chain_id = hotel.hotel_chain_id JOIN room ON hotel_chain_id = rooms.hotel_chain_id AND hotel_id = rooms.hotel_id WHERE ";
+
+        if($_GET["room_capacity"] != "") {
+            $tableQuery = $tableQuery . "and room_capacity >= $_GET["room_capacity"]";
+        }
+        if($_GET["hotel_chain"] != "") {
+            $tableQuery = $tableQuery . "and hotel_chain_name = $_GET["hotel_chain"]";
+        }
+        if($_GET[""] != "") { //TODO: what is category of hotel??
+            $tableQuery = $tableQuery . "and hotel_chain_name = $_GET[""]";
+        }
+        if($_GET[""] != "") { //TODO: this query is kind of long and complicated and I don't feel like figuring it out right now
+            $tableQuery = $tableQuery . "and hotel_chain_name = $_GET[""]";
+        }
+        if($_GET["price_of_room"] != "") {
+            $tableQuery = $tableQuery . "and price <= $_GET["price_of_room"]";
+        }
+
+
+
+        $result = pg_query($db_connection, $query) or die('Query failed: ' . pg_last_error());
+    }
+?>
 </head>
 
 <body>
@@ -11,7 +39,7 @@
 <?php include("header.php") ?>
     <!-- SECTION 1: Form -->
     <div class="container-fluid">
-        <form action="/action_page.php">
+        <form method="get">
             <div class="row">
                 <div class="col-xs-4">
                     <h1>Find a room today!</h1>
@@ -24,7 +52,7 @@
                     
                     <!-- Room Capacity -->
                     <label for="room_capacity">Room Capacity</label>
-                    <input type="usr" class="form-control" id="room_capcity">
+                    <input type="usr" class="form-control" id="room_capacity">
                     
                     <!-- Hotel Chain -->
                     <label for="hotel_chain">Hotel Chain</label>
@@ -46,6 +74,7 @@
                 </div>
             </div>
         </form>
+        <?php createTable($tableQuery, "Book Now!", "");?>
     </div>
     
 </body>
